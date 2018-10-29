1ad3df43af6233216e1b1c8b9a6790a234f22689
Allen Thomas <athoma10@ncsu.edu>
2018-10-29 09:46:12 -0400

    Created working routes and async react lifecycle methods

--- Fullstack/server/server.py
+++ Fullstack/server/server.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route("/")
 def index():
@@ -9,7 +10,7 @@ def index():
 
 @app.route("/hello")
 def hello():
-    return 'Hello World!dasdsadsa'
+    return 'lifetime'
 
 if __name__ == "__main__":
     app.run()
