d83b860891904aba347c59496bf49633bb0c1478
Allen Thomas <athoma10@ncsu.edu>
2018-10-31 00:25:59 -0400

    Added checkbox and radiobox components to the front end

--- Fullstack/server/server.py
+++ Fullstack/server/server.py
@@ -1,5 +1,5 @@
 # server.py
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -10,7 +10,13 @@ def index():
 
 @app.route("/hello")
 def hello():
-    return 'lifetime'
+    setup = jsonify({
+        "setup": {
+            "features": ['lifespan', 'emails recv'],
+            "models": ['churn-out']
+        }
+    })
+    return setup    
 
 if __name__ == "__main__":
     app.run()
