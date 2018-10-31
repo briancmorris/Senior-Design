db9849299a4ec6bec09af6e8507077e24ac0fca2
Allen Thomas <athoma10@ncsu.edu>
2018-10-31 10:43:42 -0400

    Added working front-end

--- Fullstack/server/server.py
+++ Fullstack/server/server.py
@@ -1,6 +1,7 @@
 # server.py
-from flask import Flask, render_template, jsonify
-
+import os
+from flask import Flask, render_template, jsonify, request, send_file
+from werkzeug.utils import secure_filename
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
@@ -8,15 +9,41 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def index():
     return render_template("index.html")
 
-@app.route("/hello")
+@app.route("/downloadDataFile")
+def downloadDataFile():
+    try:
+        return send_file('./Data/exampleFile.csv', as_attachment=True)
+    except Exception as e:
+        return e
+
+@app.route("/setUp")
 def hello():
     setup = jsonify({
         "setup": {
             "features": ['lifespan', 'emails recv'],
-            "models": ['churn-out']
+            "models": ['churn-out', 'lifetime value']
         }
     })
     return setup    
 
+UPLOAD_FOLDER = './Data/'
+@app.route("/results", methods=['POST'])
+def getResults():
+    print("GOT HERE")
+    if 'file' not in request.files:
+        return 'No File Uploaded'
+    print("GOT HERE")
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected File'
+    if file:
+        filename = secure_filename(file.filename)
+        print(file)
+        file.save(os.path.join(app.root_path, 'Data', filename))
+        return 'Got the file'
+    
+    
+
+    
 if __name__ == "__main__":
     app.run()
