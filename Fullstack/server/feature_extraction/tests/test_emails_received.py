2397835e9df7cd0e6657a3ab8321d482bdbd9aa2
Peter Girouard <plgiroua@ncsu.edu>
2018-11-30 11:53:45 -0500

    Fixed pytest with folder in new location, tests passing for #1 #4 #6

--- Fullstack/server/feature_extraction/tests/test_emails_received.py
+++ Fullstack/server/feature_extraction/tests/test_emails_received.py
@@ -3,13 +3,13 @@ from feature_extraction import recv
 
 class TestEmailsReceived(object):
     def test_received(self):
-        test_df = pd.read_csv('./feature_extraction/tests/test_files/test_emails_received.csv',
+        test_df = pd.read_csv('Fullstack/server/feature_extraction/tests/test_files/test_emails_received.csv',
                               dtype={'actionID': 'float'}, parse_dates=['timestamp'])
         fe_emails_received = recv.EmailsReceivedTransformer()
         trans_df = fe_emails_received.transform(test_df)
 
-        exp_received = {}
+        exp_received = {'c_00001': '1', 'c_00002': '0', 'c_00003': '1'}
         i = 0
         for key in exp_received:
-            assert exp_received[key] == str(trans_df.iloc[i,0])
+            assert exp_received[key] == str(trans_df.iloc[i, 0])
             i += 1
--- Fullstack/server/feature_extraction/tests/test_links_clicked.py
+++ Fullstack/server/feature_extraction/tests/test_links_clicked.py
@@ -3,13 +3,13 @@ from feature_extraction import links_clicked
 
 class TestLinksClicked(object):
     def test_links_clicked(self):
-        test_df = pd.read_csv('./feature_extraction/tests/test_files/test_links_clicked.csv',dtype={'actionID': 'float'}, parse_dates=['timestamp'])
+        test_df = pd.read_csv('Fullstack/server/feature_extraction/tests/test_files/test_links_clicked.csv',
+                              dtype={'actionID': 'float'}, parse_dates=['timestamp'])
         fe_links_clicked = links_clicked.TotalLinksClicked()
         trans_df = fe_links_clicked.transform(test_df)
 
         exp_clicked = {'c_00001': '1', 'c_00002': '2', 'c_00003': '3'}
         i = 0
         for key in exp_clicked:
-            print(str(trans_df.iloc[:,0]))
-            # assert exp_clicked[key] == str(trans_df.iloc[i,0])
+            assert exp_clicked[key] == str(trans_df.iloc[i])
             i = i + 1
